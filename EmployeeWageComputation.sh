27d76dc3e3d3d791bac20388bb15d40341cfc7ca
Rekhajambhulkar <jambhulkarrekha>
2020-09-27 23:27:35 +0530

    Employee is present or absent

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,4 +1,18 @@
 #! /bin/bash -x
 
+#Display welcome message to employeewage
 echo "Welcome To EmployeeWage Computation Program"
 
+#set "1" to indicate employee is present
+IS_EMPLOYEE_PRESENT=1;
+
+#variables
+Choice=$((RANDOM%2))
+
+#check condition employee is present or absent
+if [[ $IS_EMPLOYEE_PRESENT -eq $Choice ]]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
